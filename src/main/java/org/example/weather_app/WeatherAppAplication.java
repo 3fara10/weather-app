2870f25f21545144451e9cf6e2ad3f2c171db0b2
3fara10 <122831964+3fara10@users.noreply.github.com>
2025-02-21 14:25:17 +0200

    api problem

---
+++ src/main/java/org/example/weather_app/WeatherAppAplication.java
@@ -0,0 +1,12 @@
+package org.example.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppAplication.class, args);
+    }
+}
